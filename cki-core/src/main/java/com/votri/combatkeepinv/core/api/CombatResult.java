6fcce91c4379a3a107d32e5fa67a7b142795d12e
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-08-22 12:44:07 +0700

    Create CombatResult.java

---
+++ cki-core/src/main/java/com/votri/combatkeepinv/core/api/CombatResult.java
@@ -0,0 +1,18 @@
+package com.votri.combatkeepinv.core.api;
+
+public enum CombatResult {
+
+    SUCCESS,
+
+    ALREADY_IN_COMBAT,
+
+    NOT_IN_COMBAT,
+
+    PLAYER_NOT_FOUND,
+
+    INVALID_ARGUMENT,
+
+    DISABLED,
+
+    FAILED
+}
