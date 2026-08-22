ac3b4d9681b62c3589953369a72ad0918530717f
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-08-22 18:51:32 +0700

    Create PlatformCapability.java

---
+++ cki-core/src/main/java/com/votri/combatkeepinv/core/platform/PlatformCapability.java
@@ -0,0 +1,20 @@
+package com.votri.combatkeepinv.core.platform;
+
+public enum PlatformCapability {
+
+    COMBAT,
+
+    DEATH,
+
+    INVENTORY,
+
+    DAMAGE_ATTRIBUTION,
+
+    PROXY_SESSION,
+
+    SERVER_SWITCH,
+
+    CLUSTER_EXIT,
+
+    PLUGIN_MESSAGING
+}
