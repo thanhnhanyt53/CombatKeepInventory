3ef0daa176ad8df223e94b34559497e6cdae4943
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-08-23 08:49:27 +0700

    Update CombatKeepInventoryVelocity.java

--- cki-velocity/src/main/java/com/votri/combatkeepinv/velocity/CombatKeepInventoryVelocity.java
+++ cki-velocity/src/main/java/com/votri/combatkeepinv/velocity/CombatKeepInventoryVelocity.java
@@ -12,7 +12,7 @@ import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
-import javax.inject.Inject;
+import com.google.inject.Inject;
 
 @Plugin(
         id = "combatkeepinventory",
