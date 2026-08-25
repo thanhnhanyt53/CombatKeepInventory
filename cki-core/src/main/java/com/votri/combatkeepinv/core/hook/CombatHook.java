9eb046947cee21661d7f644402e4420fdc37818a
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-08-25 09:22:16 +0700

    Add files via upload

--- cki-core/src/main/java/com/votri/combatkeepinv/core/hook/CombatHook.java
+++ cki-core/src/main/java/com/votri/combatkeepinv/core/hook/CombatHook.java
@@ -1,13 +1,24 @@
 package com.votri.combatkeepinv.core.hook;
 
 import com.votri.combatkeepinv.core.api.CombatPlayer;
+import com.votri.combatkeepinv.core.api.CombatTag;
 import com.votri.combatkeepinv.core.api.DeathContext;
 
 public interface CombatHook {
 
     default void onCombatStart(
             CombatPlayer attacker,
-            CombatPlayer victim
+            CombatPlayer victim,
+            CombatTag attackerTag,
+            CombatTag victimTag
+    ) {
+    }
+
+    default void onCombatRefresh(
+            CombatPlayer attacker,
+            CombatPlayer victim,
+            CombatTag attackerTag,
+            CombatTag victimTag
     ) {
     }
 
