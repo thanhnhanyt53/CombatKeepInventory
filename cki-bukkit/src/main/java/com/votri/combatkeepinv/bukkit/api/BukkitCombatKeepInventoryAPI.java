942e4a9f47a22a9ff1128a07d8ab438c2eee5a86
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-09-13 08:57:42 +0700

    Update BukkitCombatKeepInventoryAPI.java

--- cki-bukkit/src/main/java/com/votri/combatkeepinv/bukkit/api/BukkitCombatKeepInventoryAPI.java
+++ cki-bukkit/src/main/java/com/votri/combatkeepinv/bukkit/api/BukkitCombatKeepInventoryAPI.java
@@ -1,8 +1,13 @@
 package com.votri.combatkeepinv.bukkit.api;
 
 import com.votri.combatkeepinv.bukkit.CombatKeepInventory;
+import com.votri.combatkeepinv.core.api.CombatAPI;
 import com.votri.combatkeepinv.core.api.CombatKeepInventoryAPI;
 import com.votri.combatkeepinv.core.api.CombatService;
+import com.votri.combatkeepinv.core.api.DamageAPI;
+import com.votri.combatkeepinv.core.api.DeathAPI;
+import com.votri.combatkeepinv.core.api.InventoryAPI;
+import com.votri.combatkeepinv.core.api.PlatformAPI;
 import com.votri.combatkeepinv.core.platform.PlatformInfo;
 
 import java.util.Objects;
@@ -20,6 +25,34 @@ public final class BukkitCombatKeepInventoryAPI
                 "plugin"
         );
     }
+
+    /*
+     * ==========================================================
+     * NEW API 1.2+
+     * ==========================================================
+     */
+
+    public CombatAPI getCombat() {
+        return plugin.getCombatAPI();
+    }
+
+    public DamageAPI getDamage() {
+        return plugin.getDamageAPI();
+    }
+
+    public DeathAPI getDeath() {
+        return plugin.getDeathAPI();
+    }
+
+    public InventoryAPI getInventory() {
+        return plugin.getInventoryAPI();
+    }
+
+    /*
+     * ==========================================================
+     * LEGACY API
+     * ==========================================================
+     */
 
     @Override
     public CombatService getCombatService() {
@@ -39,5 +72,19 @@ public final class BukkitCombatKeepInventoryAPI
     @Override
     public PlatformInfo getPlatform() {
         return plugin.getPlatform();
+    }
+
+    /*
+     * ==========================================================
+     * PLATFORM API
+     * ==========================================================
+     */
+
+    public PlatformAPI getPlatformAPI() {
+        return plugin.getPlatformAPI();
+    }
+
+    public CombatKeepInventory getPlugin() {
+        return plugin;
     }
 }
