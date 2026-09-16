cbf22a68589d0796be3106a44234ff3a15994d2b
thanhnhanyt53 <nguyennguyen87a@gmail.com>
2026-09-16 18:54:40 +0700

    Update CombatKeepInventoryVelocity.java

--- cki-velocity/src/main/java/com/votri/combatkeepinv/velocity/CombatKeepInventoryVelocity.java
+++ cki-velocity/src/main/java/com/votri/combatkeepinv/velocity/CombatKeepInventoryVelocity.java
@@ -32,7 +32,7 @@ import java.nio.file.StandardCopyOption;
 @Plugin(
         id = "combatkeepinventory",
         name = "CombatKeepInventory",
-        version = "1.1.0",
+        version = "1.2.0",
         authors = {
                 "ThanhNhan"
         }
